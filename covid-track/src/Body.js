cc2b31c62fcf3c286e76eb379cdf760b13d5adcb
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-05-14 15:59:09 +0530

    table added

--- covid-track/src/Body.js
+++ covid-track/src/Body.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
+import CovidTable from './component/Table/Table'
  const Body = () => {
 
     return(
         <div>
-            // body components here 
-            "body"
+           <CovidTable />
             </div>
     )
 }
