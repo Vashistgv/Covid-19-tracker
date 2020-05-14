c20f6114f915a390b34506f55a033d37dbe3ad6c
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-05-14 13:19:56 +0530

    formatted the app

---
+++ covid-track/src/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ const Body = () => {
+
+    return(
+        <div>
+            // body components here 
+            "body"
+            </div>
+    )
+}
+
+export default Body
---
+++ covid-track/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ const Footer = () => {
+
+    return(
+        <div>
+            // Footer components here 
+            "Footer"
+            </div>
+    )
+}
+
+export default Footer
