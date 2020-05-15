44fb65cf48c24630c7e9b996b080fd7aa5ea7ef9
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-05-15 12:21:28 +0530

    nav and body re structured

--- covid-track/src/App.js
+++ covid-track/src/App.js
@@ -1,23 +1,40 @@
 import React , {Suspense, lazy} from 'react';
-
-import NavBar from './component/NavBar/Nav'
+import {Layout} from 'antd';
+import NavBar from './component/NavBar/Nav';
 import FooterComponent from './component/Footer/Footer'
 import 'antd/dist/antd.css';
 
-
+const { Header, Content, Sider } = Layout;
 const Body = lazy(() => import('./Body'));
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
+    <Header  style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
 
       <NavBar/>
+      </Header>
+
+
+      <Layout style={{ padding: '0 50px', marginTop: 64 }}>
       
-      <Suspense fallback={<div className="lazy">loading...</div>} >
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+           
+          }}
+        >
+         <Suspense fallback={<div className="lazy">loading...</div>} >
         <Body />
       </Suspense>
+        </Content>
+      </Layout>
+      
       <FooterComponent />
-    </div>
+      </Layout>
   );
 }
 
--- covid-track/src/component/NavBar/Nav.js
+++ covid-track/src/component/NavBar/Nav.js
@@ -3,22 +3,11 @@ import LeftMenu from './LeftMenu'
 import RightMenu from './rightMenu'
 
 import './Nav.css';
-class Navbar extends Component {
+const  Navbar = () =>  {
 
-  render() {
-    return (
-        <nav className="menuBar">		 
-        	<div className="logo">
-        		<a href="" className="LogoImg"></a>
-        	</div>
-        	<div className="menuCon">
-        		<div className="leftMenu">
-	        		<LeftMenu />
-				    </div>
-        	</div>
-        </nav>
-    );
-  }
+
+    return 	<LeftMenu />
+				  
 }
 
 export default Navbar;
